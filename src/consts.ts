028a823ac54b589b9b7a96f19f3796341491c5c3
jarihitam <jarihitamcom@gmail.com>
2026-07-29 01:59:25 +0700

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
-  NAME: "CTRL",
+  NAME: "",
   EMAIL: "w3b.ctrl@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
@@ -9,7 +9,7 @@ export const SITE: Site = {
 };
 
 export const HOME: Metadata = {
-  TITLE: "",
+  TITLE: "CTRL",
   DESCRIPTION: "Tech, Innovation, Solutions.",
 };
 
